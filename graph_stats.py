a8288eba04e4fd6646de638c851582e905028ba0
Emma926 <emmawong926@gmail.com>
2017-03-27 14:47:35 -0400

    detect whether graph is cyclic in graph_stats.py

--- graph_stats.py
+++ graph_stats.py
@@ -41,7 +41,54 @@ computations = {'matrix':[], 'vector':[], 'real':[], 'int':[]}
 discrete = []
 continuous = []
 
+def _dfs(curr_node, graph, visiting):  
+  if curr_node in visiting:
+    return True
+  if not curr_node in graph:
+    return False
+  visiting.add(curr_node)
+  for n in graph[curr_node]:
+    if _dfs(n, graph, visiting) == True:
+      return True
+  visiting.remove(curr_node)
+  return False
+
+def dfs(leaves, graph):
+  for n in leaves:
+    if _dfs(n, graph, set()) == True:
+      return True
+  return False
+
+def bfs(leaves, graph):
+    queue = []
+    node_depth = {}
+    for i in leaves:
+      queue.append((i,1))
+      node_depth[i] = 1
+    while len(queue) > 0:
+      node, l = queue[0]
+      del queue[0]
+      for parents, dep in graph[node]:
+        for p in parents:
+          if str(p).isdigit():
+            continue
+          if p in node_depth:
+            d = node_depth[p]
+          else:
+            d = 0
+          queue.append((p, max([d, l+1])))
+          node_depth[p] = max([d, l+1])
+    max_depth = 0
+    for k,v in node_depth.iteritems():
+      if v > max_depth:
+        max_depth = v
+    return max_depth
+
+
+c = 0
 for graphfile in files:
+    print c,graphfile
+    c += 1
     with open(graphfile) as fin:
          [graph, attr, var_type] = json.load(fin)
     name = graphfile.split('stan_')[-1].split('/')[0] + '-' + graphfile.split('/')[-1].replace('.probgraph', '')
@@ -107,30 +154,23 @@ for graphfile in files:
         for p in parents:
           if p in leaves:
             leaves.remove(p)
-    # bfs
-    queue = []
-    node_depth = {}
-    for i in leaves:
-      queue.append((i,1))
-      node_depth[i] = 1
-    while len(queue) > 0:
-      node, l = queue[0]
-      del queue[0]
-      for parents, dep in graph[node]:
+
+    # check whether the graph has loops or not
+    # dfs    
+    new_graph = {}
+    for node,v in graph.iteritems():
+      new_graph[node] = set()
+      for parents, dep in v:
         for p in parents:
-          if str(p).isdigit():
-            continue
-          if p in node_depth:
-            d = node_depth[p]
-          else:
-            d = 0
-          queue.append((p, max([d, l+1])))
-          node_depth[p] = max([d, l+1])
-    max_depth = 0
-    for k,v in node_depth.iteritems():
-      if v > max_depth:
-        max_depth = v
-    layers.append(max_depth)
+          new_graph[node].add(p)
+    iscyclic = dfs(leaves, new_graph)
+    print iscyclic
+    if iscyclic:
+      layers.append(99999)
+    
+    if not iscyclic:
+      # max depth by bfs
+      layers.append(bfs(leaves, graph))
           
 print 'names = ' + str(names)
 print 'nodes = ' + str(nodes)
